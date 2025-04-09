8f1f539db040f3305bb9277d7fdb3b515c045b15
Ninjaid11 <ivakhnenkoid@gmail.com>
2025-04-09 22:58:33 +0300

    ❤️❤️❤️

--- pythonProject7/pythonProject7/myapp/admin.py
+++ pythonProject7/pythonProject7/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Student, Group, LibraryCard, BookCheckout
 # Register your models here.
+
+
+# Регистрируем модели для отображения в админке
+admin.site.register(Student)
+admin.site.register(Group)
+admin.site.register(LibraryCard)
+admin.site.register(BookCheckout)
--- pythonProject7/pythonProject7/myapp/models.py
+++ pythonProject7/pythonProject7/myapp/models.py
@@ -10,3 +10,52 @@ class Child(models.Model):
     age = models.IntegerField(default=18)
     email = models.EmailField(default=255, unique=True)
     fav_toy = models.CharField(max_length=255, blank=False, default="")
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    student_card_number = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(blank=True)
+
+    def __self__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Group(models.Model):
+    group_number = models.CharField(max_length=20)
+    gaslo = models.CharField(max_length=255, blank=True)
+    meeting_room = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.group_number
+
+
+class LibraryCard(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    issue_date = models.DateField()
+    expiration_date = models.DateField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f'Card for {self.student.first_name} {self.student.last_name}'
+
+
+class Literature(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    publication_date =models.DateField()
+    year_of_publication = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class BookCheckout(models.Model):
+    library_card = models.ForeignKey(LibraryCard, on_delete=models.CASCADE)
+    literature = models.ForeignKey(Literature, on_delete=models.CASCADE)
+    checkout_date = models.DateField()
+    librarian_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.library_card.student.first_name} {self.library_card.student.last_name} - {self.literature.title}'
