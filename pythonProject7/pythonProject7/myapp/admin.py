9a5015ada00462af89ebb6af18bc073d7e1cd281
Ninjaid11 <ivakhnenkoid@gmail.com>
2025-04-10 08:51:31 +0300

    ❤️❤️❤️

--- pythonProject7/pythonProject7/myapp/admin.py
+++ pythonProject7/pythonProject7/myapp/admin.py
@@ -3,7 +3,6 @@ from .models import Student, Group, LibraryCard, BookCheckout, Literature
 # Register your models here.
 
 
-# Регистрируем модели для отображения в админке
 admin.site.register(Student)
 admin.site.register(Group)
 admin.site.register(LibraryCard)
