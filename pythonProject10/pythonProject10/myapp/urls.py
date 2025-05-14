ce3278a4c495cc6479390e663e7808667d7d28ea
Ninjaid11 <ivakhnenkoid@gmail.com>
2025-05-14 20:35:14 +0300

    ❤️❤️❤️

--- pythonProject10/pythonProject10/myapp/urls.py
+++ pythonProject10/pythonProject10/myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, user_logout, add_product, product_lists_admin
+from .views import home, user_logout, add_product, product_lists_admin, register, login_view
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', login_view, name='login'),
+    path('register/', register, name='register'),
     path('logout/', user_logout, name='logout'),
     path('add/', add_product, name='add_product'),
     path('lists/', product_lists_admin, name='product_lists_admin'),
--- pythonProject10/pythonProject10/myapp/views.py
+++ pythonProject10/pythonProject10/myapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
-from .forms import ProductForm
+from django.contrib.auth import logout, login, authenticate
+from .forms import ProductForm, RegisterForm
 from .models import Product
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 
 # views.py
@@ -13,6 +15,30 @@ def home(request):
         'top_products': top_products,
         'main_products': main_products,
     })
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 
 def user_logout(request):
