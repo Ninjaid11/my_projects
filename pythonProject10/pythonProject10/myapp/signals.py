9fdc15d5b02d1c01272429eaf0df7ad84356e573
Ninjaid11 <ivakhnenkoid@gmail.com>
2025-05-21 22:33:37 +0300

    ❤️❤️❤️

--- pythonProject10/pythonProject10/myapp/signals.py
+++ pythonProject10/pythonProject10/myapp/signals.py
@@ -2,3 +2,13 @@ from django.core.mail import send_mail
 from django.dispatch import Signal, receiver
 
 order_completed = Signal()
+
+
+@receiver(order_completed)
+def handle_order_completed(sender, order, **kwargs):
+    send_mail(
+        subject="Your order approved",
+        message=f"Thanks for your trust! Your order ID is {order.id}.",
+        from_email=None,
+        recipient_list=[order.user.email]
+    )
